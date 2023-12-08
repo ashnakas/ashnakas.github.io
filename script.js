d489ba97d6e2f12a94c9b5c14ccff938743db0ed
ashnakas <114828820+ashnakas@users.noreply.github.com>
2023-12-08 13:59:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+function showSection(sectionId) {
+    // Select all sections
+    const sections = document.querySelectorAll("body > div");
+
+    // Hide all sections
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Show the selected section
+    document.getElementById(sectionId).style.display = 'block';
+}
+
+// Initial setup to show the about section by default
+window.onload = function() {
+    showSection('about');
+};
